d4f96e7a694bfa79b43d8f9c7ab8991e9ddbfa9c
Rafael Alfa <rafael.mediario@gmail.com>
2021-09-11 15:24:48 -0300

    Add meta tags

--- assets/head.js
+++ assets/head.js
@@ -1,5 +1,10 @@
 import { render } from './JSElements.js';
 
+let metaTags = document.createDocumentFragment()
+
+metaTags.appendChild(meta({'http-equiv': 'X-UA-Compatible', content: 'IE=edge'}))
+metaTags.appendChild(meta({name: 'viewport', content: 'width=device-width, initial-scale=1.0'}))
+
 let googleapis      = link({rel: "preconnect", href: "https://fonts.googleapis.com"})
 let gstatic         = link({rel: "https://fonts.gstatic.com", href: "https://fonts.googleapis.com", crossorigin: true})
 let googleFontGlory = link({href: "https://fonts.googleapis.com/css2?family=Glory:wght@200;600&display=swap", rel: "stylesheet"})
@@ -11,6 +16,8 @@ let mainStyle       = link({href: './assets/mainStyle.less', type: 'text/css', r
 let less            = script({src: 'https://cdn.jsdelivr.net/npm/less@4.1.1',  type: 'text/javascript'})
 
 render(document.head,
+  metaTags,
+
   googleapis,
   gstatic,
   googleFontGlory,
