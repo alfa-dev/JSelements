25d5140b6f69e62b30acebdb77f398e9352af9e9
Rafael Alfa <rafael.mediario@gmail.com>
2021-09-11 01:26:12 -0300

    Adjusting the main page style

--- assets/JSElements.js
+++ assets/JSElements.js
@@ -38,13 +38,9 @@ tags.map(function(e) {
 
     function isElement(obj) {
       try {
-        //Using W3 DOM2 (works for FF, Opera and Chrome)
         return obj instanceof HTMLElement;
       }
       catch(e){
-        //Browsers not supporting W3 DOM2 don't have HTMLElement and
-        //an exception is thrown and we end up here. Testing some
-        //properties that all elements have (works on IE7)
         return (typeof obj==="object") &&
           (obj.nodeType===1) && (typeof obj.style === "object") &&
           (typeof obj.ownerDocument ==="object");
@@ -58,5 +54,11 @@ tags.map(function(e) {
 window['render'] = function(...content) {
   content.map(function(e) {
     document.body.appendChild(e);
+  });
+}
+
+window['head'] = function(...content) {
+  content.map(function(e) {
+    document.head.appendChild(e);
   });
 }
